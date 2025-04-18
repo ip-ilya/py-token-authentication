53a4166d99a4698a12cab89be9155e9de9daae83
Illia Khomutov <ilyakhomutov613@gmail.com>
2025-04-19 01:42:30 +0300

    Solution 2

--- cinema/urls.py
+++ cinema/urls.py
@@ -11,13 +11,46 @@ from cinema.views import (
 )
 
 router = routers.DefaultRouter()
-router.register("genres", GenreViewSet)
-router.register("actors", ActorViewSet)
-router.register("cinema_halls", CinemaHallViewSet)
-router.register("movies", MovieViewSet)
 router.register("movie_sessions", MovieSessionViewSet)
-router.register("orders", OrderViewSet)
 
-urlpatterns = [path("", include(router.urls))]
+list_create_actions = {
+    "get": "list",
+    "post": "create"
+}
+
+urlpatterns = [
+    path("", include(router.urls)),
+    path(
+        "genres/",
+        GenreViewSet.as_view(actions=list_create_actions),
+        name="genre-list"
+    ),
+    path(
+        "cinema_halls/",
+        CinemaHallViewSet.as_view(actions=list_create_actions),
+        name="cinemahall-list"
+    ),
+    path(
+        "actors/",
+        ActorViewSet.as_view(actions=list_create_actions),
+        name="actor-list"
+    ),
+    path(
+        "movies/",
+        MovieViewSet.as_view(actions=list_create_actions),
+        name="movie-list"
+    ),
+    path(
+        "movies/<int:pk>/",
+        MovieViewSet.as_view(actions={"get": "retrieve"}),
+        name="movie-detail"
+    ),
+    path(
+        "orders/",
+        OrderViewSet.as_view(actions=list_create_actions),
+        name="order-list"
+    )
+
+]
 
 app_name = "cinema"
